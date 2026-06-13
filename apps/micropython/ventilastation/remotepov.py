7e05797f71102bcea409ebdc652bd1aa0d5bbcfd
fernandogcat <hi@fernandog.cat>
2026-06-13 18:38:49 +0200

    Re-send palettes and imagestrips when a client connects

--- apps/micropython/ventilastation/remotepov.py
+++ apps/micropython/ventilastation/remotepov.py
@@ -5,6 +5,8 @@ import gc
 
 sprite_data = bytearray(b"\0\0\0\xff\xff" * 100)
 stripes = {}
+_palettes = None
+_last_conn = None
 
 def init(num_pixels, *hw_config):
     pass
@@ -14,7 +16,9 @@ def set_palettes(palette):
     # it's not the case in the actual platform so I added a collect here.
     # Please, remove if it's no-bueno.
     gc.collect()
-    
+
+    global _palettes
+    _palettes = palette
     comms.send(b"palette %d" % (len(palette)/ 1024),  palette)
 
 def getaddress(sprite_num):
@@ -36,7 +40,20 @@ def set_imagestrip(n, stripmap):
     stripes[n] = stripmap
     comms.send(b"imagestrip %s %d" % (n, len(stripmap)), stripmap)
 
+def resend_all():
+    # Re-send the full current asset set so a client that connects mid-run
+    # (at any time) receives palettes + imagestrips and can render immediately.
+    if _palettes is not None:
+        comms.send(b"palette %d" % (len(_palettes) / 1024), _palettes)
+    for n in stripes:
+        comms.send(b"imagestrip %s %d" % (n, len(stripes[n])), stripes[n])
+
 def update():
+    global _last_conn
+    c = comms.conn
+    if c is not None and c is not _last_conn:
+        _last_conn = c
+        resend_all()
     comms.send(b"sprites", sprite_data)
 
 def last_turn_duration():
